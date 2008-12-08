da8810818d8cd7bf4805dfbb8375e199457a9d3d
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-08 18:48:52 +0100

    Doc client arrive...

--- src/client/client_doc.c
+++ src/client/client_doc.c
@@ -7,17 +7,36 @@ void client_doc_syntaxe(char *name) {
 /*!
  * \brief Affichage de la liste des commandes
  */
-void client_doc_commandes();
+void client_doc_commandes() {
+	fprintf(stdout, "[Commandes disponibles]\n"
+			"  ouvrir, fermer, livre, auteur, emprunter, rendre, quitter\n");
+}
 
 /*!
- * \brief Affichage de la syntaxe de la commande d'ouverture de session
+ * \brief Affichage de la syntaxe d'ouverture de session
  */
-void client_doc_emprunt();
+void client_doc_ouvrir() {
+	fprintf(stdout, "[ouvrir]\n"
+			"  Bonne question ! Rien dans la doc...\n");
+
+}
+
+/*!
+ * \brief Affichage de la syntaxe de fermeture de session
+ */
+
+void client_doc_fermer() {
+	fprintf(stdout, "[fermer]\n"
+			"  Ferme la session en cours\n");
+}
 
 /*!
  * \brief Affichage de la syntaxe de l'emprunt de livre
  */
-void client_doc_emprunt();
+void client_doc_emprunt() {
+	fprintf(stdout, "Usage : emprunter a nb\n"
+			"Emprunte le livre de référence nb.");
+}
 
 /*!
  * \brief Affichage de la syntaxe de la consultation d'auteur
@@ -45,8 +64,3 @@ void client_doc_adherent();
  */
 void client_doc_session();
 
-/*!
- * \brief Affichage de la syntaxe de fermeture de session
- */
-
-void client_doc_fermeture();
