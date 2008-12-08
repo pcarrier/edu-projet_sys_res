dea0b1e56630fb2449129fc8cb959cdc4946617c
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-08 11:51:47 +0100

    getline()

---
+++ src/prompt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+char *getline() {
+	char *line;
+	line = readline("> ");
+	add_history(line);
+	return(line);
+}
