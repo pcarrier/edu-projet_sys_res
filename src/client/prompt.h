068406278867274d19cd6631d3b570db031a18f3
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-08 12:19:10 +0100

    Hop hop hop ! On structure jeunes gens !

---
+++ src/client/prompt.h
@@ -0,0 +1,11 @@
+/*!
+ * \file prompt.h
+ * \brief Interactions à travers un prompt
+ */
+
+/*!
+ * \brief Obtenir une ligne à travers une invite de commande
+ *
+ * Récupère une ligne, la place dans l'historique et la renvoie.
+ */
+char *getline();
