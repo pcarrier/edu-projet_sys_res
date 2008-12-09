2c24727be3a15c352b976a87a7c4a7a86e420c66
Saad-Dupuy Jean-Christophe <saad.dupuy@yaahoo.fr>
2008-12-09 12:48:19 +0100

    Utilisation d'un getopt plus propre.

--- src/serveur/serveur.c
+++ src/serveur/serveur.c
@@ -21,14 +21,17 @@
 
 #include <reseau/fon.h>		/* Primitives de la boite a outils */
 #include <reseau/protocole.h>	/* Types pour la communication client <-> serveur */
+
 #include <common.h>
+#include <donnees/bdd.h>
+
 #include <gestion/gestionadherents.h>
 #include <gestion/gestionlivres.h>
 #include <ctype.h>
 #include <unistd.h>
 void serveur_appli (char *service, char *protocole);	/* programme serveur */
 
-static char service_courant[PORT_LMAX] = PORT_DEFAUT;
+static char port_courant[PORT_LMAX] = PORT_DEFAUT;
 static char protocole_courant[PROTOCOL_LMAX] = PROTOCOLE_DEFAUT;
 
 /*------------------------------------------------------------------*/
@@ -40,49 +43,6 @@ void serveur_doc_syntaxe(char *progname){
 			"-p port: utiliser le port p plutot que " PORT_DEFAUT "\n"
 			"-t utiliser TPC plutot que UDP\n", progname);
 }
-/*
-void
-Usage ()
-{
-  printf
-    ("-------------------------------------------------------------------------------------------------\n");
-  printf
-    ("Usage : %s serveur(nom ou @IP) service (nom ou port)  protocole (tcp ou udp)\n",
-     argv[0]);
-  printf
-    ("Tous les arguments sont optionnels, si omis, des valeurs par défaut sont utilisés.\n");
-  printf
-    ("-------------------------------------------------------------------------------------------------\n");
-
-}
-*/
-
-/*
-int
-serveur_init (int *argc, char *argv[])
-{
-  // printf ("%d\n", *argc);
-  // Permet de passer un nombre de parametre variable a l'executable /
-  switch (*argc)
-    {
-    case 1:
-      break;
-    case 2:
-      strcpy (service_courant, argv[1]);
-      break;
-    case 3:
-      strcpy (service_courant, argv[1]);
-      strcpy (protocole_courant, argv[2]);
-      break;
-    default:
-      printf ("Usage:serveur service (nom ou port) protocole (tcp ou udp)\n");
-      exit (1);
-    }
-  printf ("Utilisation de service : %s\n", service_courant);
-  printf ("Utilisation de protocole : %s\n\n", protocole_courant);
-  return 0;
-}
-*/
 /*------------------------------------------------------------------*/
 void
 traitement_serveur ()
@@ -153,29 +113,38 @@ traitement_serveur ()
 int
 main (int argc, char **argv)
 {
-  int c;
-  char * portValue=NULL;
-
-  while((c=getopt(argc, argv, "t:p:"))!=1)
-  {
-      	 strcpy(protocole_courant,"udp");
-
-	  switch(c){
-		  case('t'):
-      			strcpy(protocole_courant,"tcp");
-			break;
-		  case('p'):
-      			strcpy(service_courant ,optarg);
-			break;
-	  }
+  int c, tflag = 0, index;
+  while ((c = getopt (argc, argv, "p:t")) != -1) {
+    switch (c) {
+    case 't':
+      tflag = 1;
+      break;
+    case 'p':
+      strcpy(port_courant, optarg);
+      break;
+    case '?':
+      if (optopt == 'p')
+	fprintf (stderr, "Option -%c requiert un argument.\n", optopt);
+      else if (isprint (optopt))
+	fprintf (stderr, "Option '-%c' inconnue.\n", optopt);
+      else
+	fprintf (stderr,
+		 "Caractère d'option '\\x%x'.\n inconne.", optopt);
+      return EXIT_FAILURE;
+    }
   }
-  printf("Port %s , Protocole : %s \n", service_courant, protocole_courant); 
+  if(tflag)
+    strcpy(protocole_courant, "tcp");
+  else
+    strcpy(protocole_courant, "udp");
+
+  printf("Port %s , Protocole : %s \n", port_courant, protocole_courant); 
   /* Initialisation de la partie réseau du client */
   //serveur_init (&argc, argv);
   /* Lancement de l'interface client */
-  traitement_serveur ();
+  // traitement_serveur ();
 
-  return 0;
+  return EXIT_SUCCESS;
 
 }
 
