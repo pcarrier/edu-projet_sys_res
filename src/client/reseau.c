af1bd3d6b05a121aa9338111443350cb290359c8
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-09 21:58:51 +0100

    Arrivee UDP dans le client

--- src/client/reseau.c
+++ src/client/reseau.c
@@ -3,11 +3,11 @@
 
 extern int client_socket;
 extern prot_params_t prot_params;
+struct sockaddr_in sa;
 
 void
 client_creer_socket ()
 {
-  struct sockaddr_in sa;
   client_socket = h_socket (AF_INET, (prot_params.type == sock_tcp) ? SOCK_STREAM : SOCK_DGRAM);
   adr_socket (prot_params.port, prot_params.host, (prot_params.type == sock_tcp) ? "tcp" : "udp", &sa, CLIENT);
   if(prot_params.type == sock_udp)
@@ -24,12 +24,20 @@ client_fermer_socket ()
 void
 client_envoyer_requete(prot_requete_t * req)
 {
-  h_writes(client_socket,(char*)req,sizeof(prot_requete_t));
+  if(prot_params.type == sock_udp)
+    {
+    }
+  else
+    h_writes(client_socket,(char*)req,sizeof(prot_requete_t));
 }
 
 prot_reponse_t
 client_recevoir_reponse() {
   prot_reponse_t rep;
-  h_reads(client_socket,(char*)(&rep),sizeof(prot_reponse_t));
+  if(prot_params.type == sock_udp)
+    {  
+    }
+  else
+    h_reads(client_socket,(char*)(&rep),sizeof(prot_reponse_t));
   return rep;
 }
