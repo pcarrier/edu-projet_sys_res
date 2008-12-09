45872895164c22bab41ce3fea1fd448ba0df7f75
mickael Scheer <s_mickael@hotmail.com>
2008-12-09 23:03:39 +0100

    ajout trait_consulter_adherent

--- src/serveur/traitement.h
+++ src/serveur/traitement.h
@@ -4,17 +4,23 @@
 #include <reseau/protocole.h>
 
 /** 
- * Cette fonction remplit un tableau (le second paramètre passé)
- * en recherchant les occurences du premier paramètre dans le nom des livres.
+ * Cette fonction remplit un tableau (le dernier paramètre passé)
+ * en recherchant les occurences du second paramètre dans le nom des livres.
  * Retourne un code prédéfini en fonction de la réussite ou du type d'échec.
 */
 prot_ret_e trait_consulter_titre(char * catalogue, char * nom_livre, livre_t * tab_result);
 
 /** 
- * Cette fonction remplit un tableau (le second paramètre passé)
- * en recherchant ls occurances du premier paramètre dans les noms d'auteurs
+ * Cette fonction remplit un tableau (le dernier paramètre passé)
+ * en recherchant les occurences du second paramètre dans les noms d'auteurs
  * Retourne un code prédéfini en fonction de la réussite ou du type d'échec.
  */
 prot_ret_e trait_consulter_auteur(char * catalogue, char * nom_auteur, livre_t * tab_result);
 
+/** 
+ * Cette fonction remplit un tableau (le dernier paramètre passé)
+ * en recherchant les occurences du second paramètre dans les noms d'adhérents.
+ * Retourne un code préféni en fonction de le réussite ou du type d'échec
+ */
+prot_ret_e trait_consulter_adherent(char * annuaire, char * nom, adherent_t * tab_result);
 #endif
