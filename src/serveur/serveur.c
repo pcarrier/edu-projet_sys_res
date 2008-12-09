c591e2a64cceb0856ab848a47b2f9140b161372b
Saad-Dupuy Jean-Christophe <saad.dupuy@yaahoo.fr>
2008-12-09 15:11:53 +0100

    Ajout d'un support de BASE du tcp.

--- src/serveur/serveur.c
+++ src/serveur/serveur.c
@@ -29,7 +29,6 @@
 #include <gestion/gestionlivres.h>
 #include <ctype.h>
 #include <unistd.h>
-
 void serveur_appli (char *service, char *protocole);	/* programme serveur */
 
 //static char port_courant[PORT_LMAX] = PORT_DEFAUT;
@@ -40,15 +39,57 @@ prot_params_t prot_params;
 /* SERVEUR															*/
 /*------------------------------------------------------------------*/
 
-void serveur_doc_syntaxe(char *progname){
-	fprintf(stderr, "Usage : %s [-p port] [-t]\n"
-			"-p port: utiliser le port p plutot que " PORT_DEFAUT "\n"
-			"-t utiliser TPC plutot que UDP\n", progname);
+void
+serveur_doc_syntaxe (char *progname)
+{
+  fprintf (stderr, "Usage : %s [-p port] [-t]\n"
+	   "-p port: utiliser le port p plutot que " PORT_DEFAUT "\n"
+	   "-t utiliser TPC plutot que UDP\n", progname);
 }
+
 /*------------------------------------------------------------------*/
 void
 traitement_serveur ()
 {
+  //Socket file descriptor.
+  int sockfd;
+  //sockaddr serveur
+  struct sockaddr_in sa;
+  char hostname[26];
+  if (prot_params.type == sock_tcp)
+    {
+      gethostname(hostname,25);
+      //Création de la socket
+      sockfd = h_socket (AF_INET, SOCK_STREAM);
+      //Renseignement des adresses de la socket
+      adr_socket (prot_params.port, hostname, "tcp", &sa, SERVEUR);
+      //Association...
+      h_bind (sockfd, &sa);
+
+      h_listen(sockfd, 10);
+      int client;
+      struct sockaddr_in addr_client;
+      client=h_accept(sockfd, &addr_client);
+      char buffer="\0";
+
+      while(buffer!="\n")
+      {
+      h_reads(client, &buffer, 1);
+      printf("Recu : %c \n", buffer);
+      h_writes(client, &buffer, 1);
+      }
+      h_close(client); 
+    }
+
+  printf ("Serveur lancé sur le port %s ", prot_params.port);
+  if (prot_params.type == sock_tcp)
+    {
+      printf (" en tcp.\n");
+    }
+  else
+    {
+      printf (" en udp.\n");
+    }
 /* A COMPLETER */
 
 #if 0				/* Code non compilé jusqu'au #endif correspondant */
@@ -105,7 +146,6 @@ traitement_serveur ()
   /* ... */
 #endif /* fin du code non compilé */
 
-
 }
 
 /*----------------------------------------------------------*/
@@ -116,36 +156,40 @@ int
 main (int argc, char **argv)
 {
 
-  int c, tflag = 0, index;
-  while ((c = getopt (argc, argv, "p:t")) != -1) {
-    switch (c) {
-    case 't':
-      tflag = 1;
-      break;
-    case 'p':
-      strcpy(prot_params.port, optarg);
-      break;
-    case '?':
-      if (optopt == 'p')
-	fprintf (stderr, "Option -%c requiert un argument.\n", optopt);
-      else if (isprint (optopt))
-	fprintf (stderr, "Option '-%c' inconnue.\n", optopt);
-      else
-	fprintf (stderr,
-		 "Caractère d'option '\\x%x'.\n inconne.", optopt);
-      return EXIT_FAILURE;
-    }
-  }
-  if(tflag)
-   prot_params.type=sock_tcp;
-  else
-   prot_params.type=sock_udp;
+  int c, tflag = 0;
+  prot_params.port = PORT_DEFAUT;
 
-  printf("Port %i , Protocole : %i \n", prot_params.type, prot_params.port); 
-  /* Initialisation de la partie réseau du client */
-  //serveur_init (&argc, argv);
+  while ((c = getopt (argc, argv, "tp:")) != -1)
+    {
+      switch (c)
+	{
+	case 't':
+	  tflag = 1;
+	  break;
+	case 'p':
+	  prot_params.port = optarg;
+	  break;
+	case '?':
+	  if (optopt == 'p')
+	    fprintf (stderr, "Option -%c requiert un argument.\n", optopt);
+	  else if (isprint (optopt))
+	    fprintf (stderr, "Option '-%c' inconnue.\n", optopt);
+	  else
+	    fprintf (stderr,
+		     "Caractère d'option '\\x%x'.\n inconne.", optopt);
+	  return EXIT_FAILURE;
+	}
+    }
+  if (tflag)
+    {
+      prot_params.type = sock_tcp;
+    }
+  else
+    {
+      prot_params.type = sock_udp;
+    }
   /* Lancement de l'interface client */
-  // traitement_serveur ();
+  traitement_serveur ();
 
   return EXIT_SUCCESS;
 
