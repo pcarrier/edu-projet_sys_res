cba0dc7294590e63ab616b22117ed3ad140a14ce
mickael Scheer <s_mickael@hotmail.com>
2008-12-09 21:13:32 +0100

    modif traitement.c et .h

--- src/serveur/traitement.h
+++ src/serveur/traitement.h
@@ -3,14 +3,16 @@
 
 /** 
  * Cette fonction remplit un tableau (le second paramètre passé)
- * en recherchant les occurences du premier paramètre dans le nom des livres
+ * en recherchant les occurences du premier paramètre dans le nom des livres.
+ * Retourne un code prédéfini en fonction de la réussite ou du type d'échec.
 */
-void trait_consulter_titre(char * annuaire, char * nom_livre, livre_t * tab_result);
+prot_ret_e trait_consulter_titre(char * catalogue, char * nom_livre, livre_t * tab_result);
 
 /** 
  * Cette fonction remplit un tableau (le second paramètre passé)
  * en recherchant ls occurances du premier paramètre dans les noms d'auteurs
+ * Retourne un code prédéfini en fonction de la réussite ou du type d'échec.
  */
-void trait_consulter_auteur(char * annuaire, char * nom_auteur, livre_t * tab_result);
+prot_ret_e trait_consulter_auteur(char * catalogue, char * nom_auteur, livre_t * tab_result);
 
 #endif
