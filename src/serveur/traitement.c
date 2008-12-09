be77f4b7022e89145130a93d569f46204d8d3c4c
mickael Scheer <s_mickael@hotmail.com>
2008-12-09 22:21:47 +0100

    modif traitement.c

--- src/serveur/traitement.c
+++ src/serveur/traitement.c
@@ -19,10 +19,10 @@ trait_consulter_titre(char * catalogue, char * nom_livre, livre_t * tab_result){
   if (retour == 0){
     int j = 0;
     int i;
-    for (i = 0 ; i < cat_nb_livre ; i++){
+    for (i = 0 ; i < cat_nb_livres ; i++){
       //on recherche la chaine de caractère reçue en paramètre parmis tous les noms de livres
       //si une occurence est trouvée, elle est rajoutée au tableau de résultat
-      if (strcasestr (Catalogue[i].titre, nom_livre) != 0 ){
+      if (strstr (Catalogue[i].titre, nom_livre) != 0 ){
 	tab_result[j] = Catalogue[i];
 	j++;
       }
@@ -51,10 +51,10 @@ trait_consulter_auteur(char * catalogue, char * nom_auteur, livre_t * tab_result
   if (retour == 0){
     int j = 0;
     int i;
-    for (i = 0; i < cat_nb_livre ; i++){
+    for (i = 0; i < cat_nb_livres ; i++){
       //on recherche la chaine de caractère reçue en paramètre parmis tous les noms d'auteurs
       //si une occurence est trouvé, elle est rajoutée au tableau de résultat
-      if ((strcasestr(Catalogue[i].auteur, nom_auteur) != 0) || (strcasestr(nom_auteur, Catalogue[i].auteur) != 0)){
+      if ((strstr(Catalogue[i].auteur, nom_auteur) != 0) || (strstr(nom_auteur, Catalogue[i].auteur) != 0)){
 	tab_result[j] = Catalogue[i];
 	j++;
       }
